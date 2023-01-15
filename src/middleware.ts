886918d8eec913dd126cc8e484422efc4d1cae05
Nguyen Quang Lam <lam.nguyen@employmenthero.com>
2023-01-15 16:11:46 +0700

    chore: add debug for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 const PUBLIC_FILE = /\.(.*)$/;
 
 export async function middleware(req: NextRequest) {
+  console.log(req.nextUrl);
+  console.log(
+    "Will not redirect",
+    req.nextUrl.pathname.startsWith("/_next") ||
+      req.nextUrl.pathname.includes("/api/") ||
+      req.headers.has("x-prerender-revalidate") ||
+      PUBLIC_FILE.test(req.nextUrl.pathname)
+  );
   if (
     req.nextUrl.pathname.startsWith("/_next") ||
     req.nextUrl.pathname.includes("/api/") ||
@@ -12,6 +20,7 @@ export async function middleware(req: NextRequest) {
     return;
   }
 
+  console.log("Will redirect", req.nextUrl.locale === "default");
   if (req.nextUrl.locale === "default") {
     const locale = req.cookies.get("NEXT_LOCALE")?.value || "en";
     return NextResponse.redirect(
