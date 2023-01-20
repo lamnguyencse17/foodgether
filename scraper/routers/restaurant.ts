a3d858b1defc0d7c5c6f33df2b444014c7b7c3b3
Nguyen Quang Lam <lam.nguyen@employmenthero.com>
2023-01-20 11:17:15 +0700

    chore: add lint workflow and trim packages

--- scraper/routers/restaurant.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getRestaurantHandler } from "../handlers/restaurant";
-
-const restaurantRouter = express.Router();
-
-restaurantRouter.post("/", getRestaurantHandler);
-
-export default restaurantRouter;
